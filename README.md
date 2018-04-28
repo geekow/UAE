c872d0cbfeb27d59ab8d8a9621cde71b277de5f7
Jean Jacobi <jean.jacobi@live.de>
2018-04-28 18:56:26 +0200

    First version out

--- README.md
+++ README.md
@@ -11,6 +11,47 @@
 docker-compose up
 ```
 
+# Api routes
+
+- On errors the api will respond a json like this:
+```json
+{'status': 'error', 'message': $ERROR_MSG}
+```
+
+Add a user translation
+------
+```bash
+curl -X POST -i http://$HOSTNAME/add_user_trad_word --data '{
+  "arabic": $ARABIC_WORD,
+  "english": $ENGLISH_WORD"
+}
+'
+```
+
+Ask a word translation
+------
+
+```bash
+curl -X GET -i http://$HOSTNAME/ask_traduction/arabic/testenglish 
+```
+- Word translation answer:
+```json
+{"status": "success", "result": $TRANSLATED_WORD}
+```
+
+Get a word that require translation
+------
+
+```bash
+curl -X GET -i http://$HOSTNAME/get_word_to_traduce/$LANGUAGETOTRADUCE
+```
+- JSON Answer:
+```json
+{"status": "success", "result": $WORD_TO_TRADUCE}
+```
+
 ### Useful information's:
 
 - UAE Api xml content in the folder UAE
+
+- Translation are case sensitive for the moment
--- uaebackend/app.py
+++ uaebackend/app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, request
+from collections import Counter
 from gevent.wsgi import WSGIServer
+from uaebackend import config
+from pymongo import MongoClient
 from logging import handlers
 import logging
+import json
 import sys
 
 file_handler = handlers.TimedRotatingFileHandler(filename='./logs/backend.logs', when='midnight', interval=1)
@@ -21,10 +25,83 @@ logger.addHandler(ch)
 app = Flask(__name__)
 app.config.update(DEBUG=True)
 
+MONGO_CLIENT = MongoClient(config.MONGO_HOST, config.MONGO_PORT)
+MONGO_DB = MONGO_CLIENT['UAE']
+
 
 @app.route('/')
 def index():
     return 'Index Page'
+
+
+@app.route('/add_user_trad_word', methods=['POST'])
+def user_trad():
+    try:
+        content = request.get_json(force=True)
+        if 'arabic' not in content or 'english' not in content:
+            return json.dumps({'status': 'error', 'message': 'Missing key(s) in JSON'})
+        # Treating arabic collections
+        result = MONGO_DB['ARABIC'].find_one({'word': content['arabic']})
+        if not result:
+            arabic = {'word': content['arabic'], 'translations': [content['english']]}
+            MONGO_DB['ARABIC'].insert_one(arabic)
+            logger.info('Successfully added translation to arabic collection')
+        else:
+            translations = result['translations']
+            translations.append(content['english'])
+            MONGO_DB['ARABIC'].update_one({'_id': result['_id']}, {'$set': {'translations': translations}},
+                                          upsert=False)
+            logger.info('Successfully updated translation to arabic collection')
+        # Treating english connections
+        result = MONGO_DB['ENGLISH'].find_one({'word': content['english']})
+        if not result:
+            english = {'word': content['english'], 'translations': [content['english']]}
+            MONGO_DB['ENGLISH'].insert_one(english)
+            logger.info('Successfully added translation to english collection')
+        else:
+            translations = result['translations']
+            translations.append(content['arabic'])
+            MONGO_DB['ENGLISH'].update_one({'_id': result['_id']}, {'$set': {'translations': translations}},
+                                           upsert=False)
+            logger.info('Successfully updated translation to english collection')
+        return json.dumps({'status': 'success'})
+    except Exception as e:
+        logger.warning('Unhandled exception: %s', e)
+        return json.dumps({'status': 'error', 'message': 'Unhandled exception, well done jjacobi !'})
+
+
+@app.route('/ask_traduction/<language>/<word>', methods=['GET'])
+def ask_trad(language, word):
+    language = str(language).upper()
+    word = str(word)
+    if language != 'ARABIC' and language != 'ENGLISH':
+        logger.warning('User attempt to translate from %s', language)
+        return json.dumps({'status': 'error', 'message': 'Language not valid'})
+    result = MONGO_DB['ARABIC' if language == 'ENGLISH' else 'ENGLISH'].find_one({'word': word})
+    if not result:
+        logger.warning('User asked for non existing translation for %s in %s', word, language)
+        if not MONGO_DB['TOTRADUCE'].find_one({'word': word, 'traduce_in': language}):
+            MONGO_DB['TOTRADUCE'].insert_one({'word': word, 'traduce_in': language})
+        return json.dumps({'status': 'error',
+                           'message': 'We have no translation for {word} in {language} for the moment'
+                          .format(word=word, language=language)})
+    # Now return most common translation
+    word_counter = Counter(result['translations'])
+    logger.info('Sending translation for %s in %s : %s', word, language, word_counter.most_common(1))
+    return json.dumps({'status': 'success', 'result': word_counter.most_common(1)[0]})
+
+
+@app.route('/get_word_to_traduce/<language>')
+def get_word_to_traduce(language):
+    language = str(language).upper()
+    language = 'ARABIC' if language == 'ENGLISH' else 'ENGLISH'
+    result = MONGO_DB['TOTRADUCE'].find_one({'traduce_in': language})
+    if not result:
+        logger.warning('We habe no word to traduce in %s', language)
+        return json.dumps({'status': 'error', 'message': 'We have no word to traduce in ' + str(language)})
+    MONGO_DB['TOTRADUCE'].remove(result)
+    logger.info('Sending %s to traduce in %s', result['word'], result['traduce_in'])
+    return json.dumps({'status': 'success', 'word': result['word']})
 
 
 @app.route('/about')
@@ -33,5 +110,5 @@ def about():
 
 
 logger.info('Launching web server on port 5000')
-http_server = WSGIServer(('', 5000), app)
+http_server = WSGIServer(('', config.WEB_SERVER_PORT), app)
 http_server.serve_forever()
